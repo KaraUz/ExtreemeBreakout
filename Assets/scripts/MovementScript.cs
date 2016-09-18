6784ddba266e2e8a2ef0892589a0475378294117
Karolis_Uzdnavicius <uzda.kara@gmail.com>
2016-09-18 13:13:17 +0300

    some movement

---
+++ Assets/scripts/MovementScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+    private Rigidbody rb;
+    public float Speed = 10;
+	// Use this for initialization
+	void Start () {
+        rb = transform.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float direction = Input.GetAxis("Horizontal");
+        rb.MovePosition(transform.position + transform.right *direction * Speed* Time.deltaTime);
+    }
+}
