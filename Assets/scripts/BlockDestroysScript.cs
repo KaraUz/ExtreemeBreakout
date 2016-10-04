b62d6bc37f05df467947fa96dd94a58c44d8581e
Karolis_Uzdnavicius <uzda.kara@gmail.com>
2016-10-04 21:02:38 +0300

    Destroy block

---
+++ Assets/scripts/BlockDestroysScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockDestroysScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionExit(Collision col)
+    {
+        
+        if (col.collider.tag.Equals("Ball"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
