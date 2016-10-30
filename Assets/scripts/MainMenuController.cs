60291dedb752ca253059c34dd7b3b34c55f68a1a
Karolis_Uzdnavicius <uzda.kara@gmail.com>
2016-10-30 20:18:46 +0200

    MainMenu

---
+++ Assets/scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+    //public string sceneName;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
