1c4e652ba83a49cee533692a1fad57b20bdc6abd
Dominik.Lisovski <dominik.lisovski@gmail.com>
2016-11-01 21:34:42 +0200

    Wall problems

--- Assets/scripts/BallController.cs
+++ Assets/scripts/BallController.cs
@@ -10,11 +10,20 @@ public class BallController : MonoBehaviour
     {
         rb = transform.GetComponent<Rigidbody>();
         rb.velocity = new Vector3(0, 0, -40);
+        //rb.velocity = new Vector3(0, 0, -40);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        /* if (Mathf.Round(rb.velocity.y) == 0 && Mathf.Round(rb.velocity.z) == 0)
+         {
+             if (rb.velocity.x >= 0)
+                 rb.velocity = new Vector3(20, 0, -20);
+             else
+                 rb.velocity = new Vector3(-20, 0, -20);
+         }
+         else*/
         //if(rb.velocity.z==0)rb.AddForce(new Vector3(0,0,-20));
         rb.velocity = rb.velocity.normalized * 40; // needed because we are loosing speed
     }
