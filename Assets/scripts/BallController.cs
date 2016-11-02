b829e14cf2f77425e9240b07493f85aa1fd6d999
Dominik.Lisovski <dominik.lisovski@gmail.com>
2016-11-02 21:40:52 +0200

    Scene changes

--- Assets/scripts/BallController.cs
+++ Assets/scripts/BallController.cs
@@ -4,33 +4,35 @@ using System.Collections;
 public class BallController : MonoBehaviour
 {
     private Rigidbody rb;
+    private bool isFree = false;
 
     // Use this for initialization
     void Start()
     {
         rb = transform.GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(0, 0, -40);
-        //rb.velocity = new Vector3(0, 0, -40);
+    }
+
+    void Update()
+    {
+        if (!isFree && Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Ball is free");
+            isFree = true;
+            transform.parent = null;
+            rb.velocity = new Vector3(0, 0, 40);
+            rb.isKinematic = false;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        /* if (Mathf.Round(rb.velocity.y) == 0 && Mathf.Round(rb.velocity.z) == 0)
-         {
-             if (rb.velocity.x >= 0)
-                 rb.velocity = new Vector3(20, 0, -20);
-             else
-                 rb.velocity = new Vector3(-20, 0, -20);
-         }
-         else*/
-        //if(rb.velocity.z==0)rb.AddForce(new Vector3(0,0,-20));
-        rb.velocity = rb.velocity.normalized * 40; // needed because we are loosing speed
+        if (isFree)
+            rb.velocity = rb.velocity.normalized * 40; // needed because we are loosing speed
     }
 
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Velocity: " + rb.velocity);
-        //rb.AddForce(new Vector3(0, 1000, 0));
     }
 }
